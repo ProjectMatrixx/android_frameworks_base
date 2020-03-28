f7a6515a2e57a6c771c0788baa0953686a21c12a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-28 12:51:07 -0300

    FaceUnlock:  Allow changing face unlock method when locked [1/2]
    Change-Id: I9a8e812bbe869dead94d631fa0a2f53f3bf67cc6
    Signed-off-by: Master <mhmdeveloperofficial@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12951,6 +12951,12 @@ public final class Settings {
         public static final String MEDIA_CONTROLS_ALWAYS_SHOW_TIME = "media_controls_always_show_time";
 
         /**
+         * Face Unlock Method
+         * @hide
+         */
+        public static final String FACE_UNLOCK_METHOD = "face_unlock_method";
+        
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
