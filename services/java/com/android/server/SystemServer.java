77f39b284197d8dc984a3d524d415cbc727e46f7
Ido Ben-Hur <idoybh2@gmail.com>
2021-02-27 01:52:33 +0200

    [SQUASH] base: Allow scheduling always on display [1/2]
    From sunset to sunrise or at a custom time
    
    Change-Id: Ib76ecb1855ac215de6d7cd2f2346abfce6cc2214
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ==========================
     ==== Squashed commits ====
     ==========================
    
    AutoAODService: Add support for mixed time & sun modes [1/2]
    
    Also refactor some code
    
    Change-Id: I3a78dec88f532766d00e0d1d276c27dc9d3dc68f
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Account for disabled doze
    
    Change-Id: I30f52c4e3db2a27f9cde01662a25eee33e225414
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Slightly improve code and docs
    
    * Correctly link local vars and functions in docs
    * Make vars we can final
    * Don't use String.valueOf() where we don't have to
    * Should be Integer.parseInt() and not Integer.valueOf()
    
    Change-Id: I5892858c7142113ad3c9c87ddf00e58b18508207
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Improve some code
    
    And call using the right handler on settings change
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Use Calendar.add instead of Calendar.roll
    
    Roll doesn't always give the intended result which can cause an alarm to be set to the distance past/future on the end of the month
    Also make sure we cancel previous alarms before setting new ones, just incase
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Properly handle reboots & fix some logic
    
    Use shared preferences to find whether the user aborted current scheduled session
    In case that happens - don't automatically toggle AOD on boot until we passed the next alarm
    
    In addition:
    * Don't toggle AOD onTwilightStateChanged nor in mTimeChangedReceiver - they should just change the alarms
    * Properly re-init on ALL setting changes, not just mode
    * Never disable AOD on boot just because the setting is disabled
    * Never trigger doze / screen on when interactive
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Better check for doze enablement
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    Signed-off-by: elpaablo <donopwhile1@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -129,6 +129,7 @@ import com.android.server.cpu.CpuMonitorService;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.devicestate.DeviceStateManagerService;
 import com.android.server.display.AutoDimService;
+import com.android.server.display.AutoAODService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
@@ -1708,6 +1709,13 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(AutoDimService.class);
                 t.traceEnd();
             }
+
+            if (context.getResources().getBoolean(R.bool.config_dozeAlwaysOnDisplayAvailable)) {
+                t.traceBegin("AutoAODService");
+                mSystemServiceManager.startService(AutoAODService.class);
+                t.traceEnd();
+            }
+
         } catch (Throwable e) {
             Slog.e("System", "******************************************");
             Slog.e("System", "************ Failure starting core service");
