d9e44057f2d331fa1333cc514d6011b0e0611aab
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2021-11-03 00:21:19 +0100

    SystemUI: Disable ringer button if alert slider is present.
    Co-authored-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Change-Id: I9c9133a7b61a29619addbd2f6b46a701ab4b62d5
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -310,6 +310,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private boolean mHasSeenODICaptionsTooltip;
     private TunerService mTunerService;
     private ViewStub mODICaptionsTooltipViewStub;
+    private boolean mHasAlertSlider;
     @VisibleForTesting View mODICaptionsTooltipView = null;
 
     // Volume panel placement left or right
@@ -397,6 +398,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             mContext.getResources().getBoolean(R.bool.config_volumeDialogUseBackgroundBlur);
         mInteractionJankMonitor = interactionJankMonitor;
         mSecureSettings = secureSettings;
+        mHasAlertSlider =
+            mContext.getResources().getBoolean(com.android.internal.R.bool.config_hasAlertSlider);
 
         dumpManager.registerDumpable("VolumeDialogImpl", this);
 
@@ -1164,21 +1167,25 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         ((LinearLayout) mRingerDrawerContainer.findViewById(R.id.volume_drawer_options))
                 .setOrientation(isLandscape() ? LinearLayout.HORIZONTAL : LinearLayout.VERTICAL);
 
-        mSelectedRingerContainer.setOnClickListener(view -> {
-            if (mIsRingerDrawerOpen) {
-                hideRingerDrawer();
-            } else {
-                showRingerDrawer();
-            }
-        });
-        updateSelectedRingerContainerDescription(mIsRingerDrawerOpen);
+        if (!mHasAlertSlider) {
+            mSelectedRingerContainer.setOnClickListener(view -> {
+                if (mIsRingerDrawerOpen) {
+                    hideRingerDrawer();
+                } else {
+                    showRingerDrawer();
+                }
+            });
 
-        mRingerDrawerVibrate.setOnClickListener(
-                new RingerDrawerItemClickListener(RINGER_MODE_VIBRATE));
-        mRingerDrawerMute.setOnClickListener(
-                new RingerDrawerItemClickListener(RINGER_MODE_SILENT));
-        mRingerDrawerNormal.setOnClickListener(
-                new RingerDrawerItemClickListener(RINGER_MODE_NORMAL));
+            updateSelectedRingerContainerDescription(mIsRingerDrawerOpen);
+
+            mRingerDrawerVibrate.setOnClickListener(
+                    new RingerDrawerItemClickListener(RINGER_MODE_VIBRATE));
+            mRingerDrawerMute.setOnClickListener(
+                    new RingerDrawerItemClickListener(RINGER_MODE_SILENT));
+            mRingerDrawerNormal.setOnClickListener(
+                    new RingerDrawerItemClickListener(RINGER_MODE_NORMAL));
+
+        }
 
         final int unselectedColor = Utils.getColorAccentDefaultColor(mContext);
         final int selectedColor = Utils.getColorAttrDefaultColor(
