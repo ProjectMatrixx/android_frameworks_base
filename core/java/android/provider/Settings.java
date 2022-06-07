af7d302de91c46d6d23d46ad923e0097d073d16b
timjosten <timjosten@gmail.com>
2022-06-07 12:44:25 +0900

    base: Remove IME space under keyboard feature [1/2]
    Change-Id: I7e322d85d8dbd937cf657d37d0a86bdc7f371c3f
    
    base: Improve IME space feature [1/2]
    
     ** AICP Edits: add backup validators
    
    Change-Id: Ic0c514850e3318077ad0964d0c0234ceccc86c83
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6927,6 +6927,14 @@ public final class Settings {
         public static final String UDFPS_ICON = "udfps_icon";
 
         /**
+         * Gesture navbar IME space.
+         * Supported modes: 0 for normal length, 1 for narrow and 2 for hidden.
+         * Default 0.
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -7057,6 +7065,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CAMERA_FLASH_NOTIFICATION);
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION);
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION_COLOR);
+            PRIVATE_SETTINGS.add(NAVIGATION_BAR_IME_SPACE);
         }
 
         /**
