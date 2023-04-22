7f25c25d9e260f1097e7ddc009a75ed75ee598e8
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-22 16:02:40 +0800

    Statusbar Clock background chip [SQUASH]
    * Inspired by looking at few magisk/substratum module
    
    [neobuddy89: Fixed up clock paddings]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    [refactored]
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    SystemUI: Statusbar clock background chip styles [SQUASH]
    
    * reconfigure rounded corners
    * increase left/right padding
    * fix textview alignment
    
    Change-Id: I0df47219bdcef2404f3c2e5affdfa2e53ec328d2
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    SystemUI: Refactor status bar chip bg feature
    
    * code rewrite for risingOS
    
    Change-Id: Ifdabca61c2b0266a8c94fa82d4dfab58c2baee62
    
    SystemUI: Add more clock bg chip styles [1/2]
    
    also Added some color code
    
    Change-Id: Ie30e0b56e5206bcae81e58d31662697b724e5443
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -25,6 +25,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.icu.text.DateTimePatternGenerator;
 import android.os.Bundle;
@@ -118,6 +119,8 @@ public class Clock extends TextView implements
             "system:" + Settings.System.STATUS_BAR_CLOCK_COLOR;
     public static final String STATUS_BAR_CLOCK_FONT_STYLE =
             "system:" + Settings.System.STATUS_BAR_CLOCK_FONT_STYLE;
+    private static final String STATUSBAR_CLOCK_CHIP =
+            "system:" + Settings.System.STATUSBAR_CLOCK_CHIP;
 
     private int mClockSize;
     private int mClockSizeQsHeader;
@@ -158,6 +161,7 @@ public class Clock extends TextView implements
     private boolean mClockAutoHideLauncher = false;
     private boolean mClockVisibleByPolicy = true;
     private boolean mClockVisibleByUser = getVisibility() == View.VISIBLE;
+    private boolean mClockBgOn;
 
     private boolean mAttached;
     private boolean mScreenReceiverRegistered;
@@ -317,7 +321,8 @@ public class Clock extends TextView implements
                     STATUS_BAR_CLOCK_SIZE,
                     QS_HEADER_CLOCK_SIZE,
                     STATUS_BAR_CLOCK_FONT_STYLE,
-                    STATUS_BAR_CLOCK_COLOR);
+                    STATUS_BAR_CLOCK_COLOR,
+                    STATUSBAR_CLOCK_CHIP);
             mCommandQueue.addCallback(this);
             if (mShowDark) {
                 Dependency.get(DarkIconDispatcher.class).addDarkReceiver(this);
@@ -575,6 +580,9 @@ public class Clock extends TextView implements
                         TunerService.parseInteger(newValue, DEFAULT_CLOCK_COLOR);
                 updateClockColor();
                 break;
+            case STATUSBAR_CLOCK_CHIP:
+                mClockBgOn = TunerService.parseInteger(newValue, 0) > 0;
+                break;
             default:
                 break;
         }
@@ -601,6 +609,7 @@ public class Clock extends TextView implements
     @Override
     public void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint) {
         mNonAdaptedColor = DarkIconDispatcher.getTint(areas, this, tint);
+
         if (mClockColor == 0xFFFFFFFF) {
             setTextColor(mNonAdaptedColor);
         } else {
