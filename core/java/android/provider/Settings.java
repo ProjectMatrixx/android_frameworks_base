d76a7b995e62590133ef8a639c5dbb4bca721458
Ghosuto <clash.raja10@gmail.com>
2023-05-26 12:39:30 +0200

    SystemUI: allow toggle dual tone QS background [1/2]
    Change-Id: I885fc0a052374af31a55d6dc6e0fcec9e94d48ff
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6387,6 +6387,12 @@ public final class Settings {
         public static final String LOCKSCREEN_WEATHER_SELECTION = "lockscreen_weather_selection";
 
         /**
+         * Enable and Disable Dual Tone Colors QsPanel
+         * @hide
+         */
+        public static final String QS_DUAL_TONE = "qs_dual_tone";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
