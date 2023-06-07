499fed5afc005ebbccd42badae55fc62afcb83fb
Ido Ben-Hur <idoybh2@gmail.com>
2023-06-07 23:59:41 +0300

    base: services: Introduce AutoDimService [1/2]
    Exactly like AutoAODService but for REDUCE_BRIGHT_COLORS_ACTIVATED
    
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -130,6 +130,7 @@ import com.android.server.cpu.CpuMonitorService;
 import com.android.server.crdroid.CustomDeviceConfigService;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.devicestate.DeviceStateManagerService;
+import com.android.server.display.AutoDimService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
@@ -1724,6 +1725,11 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(LogcatManagerService.class);
             t.traceEnd();
 
+            if (context.getResources().getBoolean(R.bool.config_reduceBrightColorsAvailable)) {
+                t.traceBegin("AutoDimService");
+                mSystemServiceManager.startService(AutoDimService.class);
+                t.traceEnd();
+            }
         } catch (Throwable e) {
             Slog.e("System", "******************************************");
             Slog.e("System", "************ Failure starting core service");
