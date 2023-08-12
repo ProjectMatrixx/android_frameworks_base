e1470d9a9fa38ba51e311f222d452dd84134610c
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-12 15:43:30 +0800

    [SQUASHME] Remove unused reticker implementation
    * this was meant to sync reticker with headsup but was skipped since unnecessary, this is a no-op change
    
    Change-Id: Ie9917d23f9274e7ce0b9f2ebca5f2cd513b94fe3
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -217,14 +217,14 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
             if (newEntry == null) {
                 // no heads up anymore, lets start the disappear animation
                 mNotificationPanelViewController.hideReticker();
-                setShown(false, null);
+                setShown(false);
                 animateIsolation = !isExpanded();
             } else if (previousEntry == null) {
                 // We now have a headsUp and didn't have one before. Let's start the disappear
                 // animation
                 mNotificationPanelViewController.hideClearAllButton();
                 mNotificationPanelViewController.updateReticker(newEntry);
-                setShown(true, newEntry);
+                setShown(true);
                 animateIsolation = !isExpanded();
             }
             updateIsolatedIconLocation(false /* requireUpdate */);
@@ -233,7 +233,7 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
         }
     }
 
-    private void setShown(boolean isShown, NotificationEntry newEntry) {
+    private void setShown(boolean isShown) {
         if (mShown != isShown) {
             View clockView = mClockController.getClock();
             boolean isClock = clockView != null &&
