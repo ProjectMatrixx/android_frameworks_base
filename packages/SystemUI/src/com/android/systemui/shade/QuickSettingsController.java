cfbbef3bdb147e1ff22eaaff4c1e1d7d09ed0186
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-14 11:24:54 +0530

    reticker: Improve popup animation & colors

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
@@ -1001,7 +1001,6 @@ public class QuickSettingsController {
         mShadeHeaderController.setQsExpandedFraction(qsExpansionFraction);
         mShadeHeaderController.setQsVisible(mVisible);
         mPanelViewControllerLazy.get().reTickerViewVisibility();
-        mPanelViewControllerLazy.get().updateDismissAllVisibility();
     }
 
     /** */
