FabricioSC123/frameworks_base-1

SystemUI: Allow customizing footer text [1/2]
Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
@@ -145,9 +145,17 @@ public class QSFooterView extends FrameLayout implements TunerService.Tunable {
         boolean isShow = Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.OMNI_FOOTER_TEXT_SHOW, 0,
                         UserHandle.USER_CURRENT) == 1;
+        String text = Settings.System.getStringForUser(mContext.getContentResolver(),
+                        Settings.System.X_FOOTER_TEXT_STRING,
+                        UserHandle.USER_CURRENT);
         if (isShow) {
-            v.setText("#Xtended");
-            v.setVisibility(View.VISIBLE);
+            if (text == null || text == "") {
+                v.setText("Xtended");
+                v.setVisibility(View.VISIBLE);
+            } else {
+                v.setText(text);
+                v.setVisibility(View.VISIBLE);
+            }
         } else {
             v.setVisibility(View.GONE);
         }
@@ -234,6 +242,10 @@ public class QSFooterView extends FrameLayout implements TunerService.Tunable {
         super.onAttachedToWindow();
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.OMNI_FOOTER_TEXT_SHOW), false,
+                mSettingsObserver, UserHandle.USER_ALL);
+
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.X_FOOTER_TEXT_STRING), false,
                 mSettingsObserver, UserHandle.USER_ALL);
 
         final TunerService tunerService = Dependency.get(TunerService.class);

ProjectMatrixx/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
@@ -85,9 +85,17 @@ public class QSFooterView extends FrameLayout {
         boolean isShow = Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.OMNI_FOOTER_TEXT_SHOW, 0,
                         UserHandle.USER_CURRENT) == 1;
+        String text = Settings.System.getStringForUser(mContext.getContentResolver(),
+                        Settings.System.FOOTER_TEXT_STRING,
+                        UserHandle.USER_CURRENT);
         if (isShow) {
-            v.setText("#Xtended");
-            v.setVisibility(View.VISIBLE);
+            if (text == null || text == "") {
+                v.setText("Matrixx");
+                v.setVisibility(View.VISIBLE);
+            } else {
+                v.setText(text);
+                v.setVisibility(View.VISIBLE);
+            }
         } else {
             v.setVisibility(View.INVISIBLE);
         }
@@ -149,6 +157,9 @@ public class QSFooterView extends FrameLayout {
                 Settings.System.getUriFor(Settings.System.OMNI_FOOTER_TEXT_SHOW), false,
                 mSettingsObserver, UserHandle.USER_ALL);
 
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.FOOTER_TEXT_STRING), false,
+                mSettingsObserver, UserHandle.USER_ALL);
     }
 
     @Override