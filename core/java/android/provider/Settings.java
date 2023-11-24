bcdf8e552383d3f5fed09affea99dfc97bf7a3ab
mukesh22584 <mks22584@gmail.com>
2023-11-24 13:01:53 +0530

    Revert "SystemUI: Add QS tile layout settings [1/2]"
    This reverts commit 3a651327aba335d1ed3a4f94208c04526009eab7.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6032,26 +6032,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String QS_LAYOUT_COLUMNS_LANDSCAPE = "qs_layout_columns_landscape";
-
-        /**
-         * @hide
-         */
-        public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
-
-        /**
-         * @hide
-         */
-        public static final String QS_TILE_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
-
-        /**
-         * @hide
-         */
-        public static final String QS_TILE_LABEL_HIDE = "qs_tile_label_hide";
-
-        /**
-         * @hide
-         */
         public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
