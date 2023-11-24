d4fdbc863ee99927fcac8c42247605d98cda62f2
mukesh22584 <mks22584@gmail.com>
2023-11-24 13:00:28 +0530

    Revert "SystemUI: Allow to change QS tile label text size [1/2]"
    This reverts commit 658dc06856cf4797ce7fe704984d5b46b0f442d6.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6052,11 +6052,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String QS_TILE_LABEL_SIZE = "qs_tile_label_size";
-
-        /**
-         * @hide
-         */
         public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
