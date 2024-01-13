8ba8f8fc8013470b1ca8c92f3402ba2000fce3ac
minaripenguin <minaripenguin@users.noreply.github.com>
2024-01-13 15:12:06 +0800

    Lockscreen Clock: Dedupe requestLayout() calls when updating status area layout
    Change-Id: I8dc9962514592bf35c6b4fbcb0459f28800f43b4
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
@@ -290,7 +290,6 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
      * Used for status view to pass the screen offset from parent view
      */
     public void setLockscreenClockY(int clockY) {
-        updateCustomClock();
         if (mView.screenOffsetYPadding != clockY) {
             mView.screenOffsetYPadding = clockY;
             mView.post(() -> mView.updateClockTargetRegions());
@@ -492,7 +491,6 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
         if (migrateClocksToBlueprint()) {
             return;
         }
-        updateCustomClock();
         mDateWeatherView = (ViewGroup) mSmartspaceController.buildAndConnectDateView(mView);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 MATCH_PARENT, WRAP_CONTENT);
@@ -820,7 +818,6 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
     }
 
     private void setDateWeatherVisibility() {
-        updateCustomClock();
         if (mDateWeatherView != null) {
             mUiExecutor.execute(() -> {
                 if (mEnableCustomClock) {
