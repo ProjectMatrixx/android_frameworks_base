03d15174a7659a8f4db2817405741a30f4183ce6
Pranav Vashi <neobuddy89@gmail.com>
2024-01-17 16:52:51 +0530

    SystemUI: Reduce tile label size for A11 QS
    * Looking like a wow.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- core/java/com/android/internal/util/systemui/qs/QSLayoutUtils.java
+++ core/java/com/android/internal/util/systemui/qs/QSLayoutUtils.java
@@ -41,9 +41,11 @@ public class QSLayoutUtils {
    }
 
     public static float getQSTileSecondaryLabelSize(Context context) {
-        return (float) Settings.System.getIntForUser(context.getContentResolver(),
+       int labelSize = Settings.System.getIntForUser(context.getContentResolver(),
                 Settings.System.QS_TILE_SECONDARY_LABEL_SIZE,
                 13, UserHandle.USER_CURRENT);
+        if (getQsUiStyle(context) != 0) labelSize = labelSize - 2;
+        return (float) labelSize;
     }
 
    public static int getQsUiStyle(Context context) {
@@ -53,9 +55,11 @@ public class QSLayoutUtils {
    }
 
     public static float getQSTileLabelSize(Context context) {
-        return (float) Settings.System.getIntForUser(context.getContentResolver(),
+        int labelSize = Settings.System.getIntForUser(context.getContentResolver(),
                 Settings.System.QS_TILE_LABEL_SIZE,
                 15, UserHandle.USER_CURRENT);
+        if (getQsUiStyle(context) != 0) labelSize = labelSize - 2;
+        return (float) labelSize;
     }
 
    public static boolean updateLayout(Context context) {
