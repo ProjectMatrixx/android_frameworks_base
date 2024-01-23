f73375f16b23ef376c493226376f95f0c05aad8e
pimpmaneaton <shaunsibu@gmail.com>
2024-01-23 03:02:07 -0700

    Base: Ambient batterybar color options [1/2]
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7100,6 +7100,16 @@ public final class Settings {
         public static final String CUSTOM_KEYGUARD_SHOW_BATTERY_BAR_ALWAYS = "sysui_keyguard_show_battery_bar_always";
 
         /**
+         * @hide
+         */
+        public static final String CUSTOM_KEYGUARD_BATTERY_BAR_COLOR_SOURCE = "sysui_keyguard_battery_bar_color_source";
+
+        /**
+         * @hide
+         */
+        public static final String CUSTOM_KEYGUARD_BATTERY_BAR_CUSTOM_COLOR = "sysui_keyguard_battery_bar_custom_color";
+
+        /**
          * Clock font size
          * @hide
          */
@@ -7350,6 +7360,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(AMBIENT_TEXT_TYPE_COLOR);
             PRIVATE_SETTINGS.add(CUSTOM_KEYGUARD_SHOW_BATTERY_BAR);
             PRIVATE_SETTINGS.add(CUSTOM_KEYGUARD_SHOW_BATTERY_BAR_ALWAYS);
+            PRIVATE_SETTINGS.add(CUSTOM_KEYGUARD_BATTERY_BAR_COLOR_SOURCE);
+            PRIVATE_SETTINGS.add(CUSTOM_KEYGUARD_BATTERY_BAR_CUSTOM_COLOR);
         }
 
         /**
