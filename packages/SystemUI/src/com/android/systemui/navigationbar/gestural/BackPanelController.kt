5551635201628ca4d52ba1f7b2d22feacf41fc09
SuperDroidBond <superdroidbond@yahoo.com>
2024-02-01 04:07:18 +0900

    [SQUASH] Revert long swipe gesture implementation [1/2]
    Revert "Extends "Use double click effect for long swipe""
    
    This reverts commit 691f8bf7d8d7e8f8c798a871aa23a2285b5d3ad4.
    
    Revert "Use double click effect for long swipe"
    
    This reverts commit e3fd362d1a977a1891ee15270ae7efcca54e844c.
    
    Revert "SystemUI: EdgeBackGestureHandler: Cancel long swipe when cancelBack called"
    
    This reverts commit eeb44c1aa11b39572de0f07789ed4d8b14bdd5b7.
    
    Revert "SystemUI: BackPanel: Always add second arrow in front on long swipe"
    
    This reverts commit 8ce871e8f03c5f1fd8b4f00a61cd455748d77f6e.
    
    Revert "Implement edge long swipe gesture to new back gesture affordance"
    
    This reverts commit 7d1bc0bcf5efe30178b3a51d7531cb6bb50c2ae1.
    
    Revert "Implement edge long swipe gesture to new back gesture affordance"
    
    This reverts commit 3203c38a38ba6a8f5dd75fa716eda5c4566c757d.
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/BackPanelController.kt
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/BackPanelController.kt
@@ -181,10 +181,6 @@ internal constructor(
 
     private val failsafeRunnable = Runnable { onFailsafe() }
 
-    private var longSwipeThreshold = 0f
-    private var triggerLongSwipe = false
-    private var isLongSwipeEnabled = false
-
     private var backArrowVisibility = false
 
     private var edgeHapticEnabled = false
@@ -312,16 +308,13 @@ internal constructor(
                 startIsLeft = mView.isLeftPanel
                 hasPassedDragSlop = false
                 mView.resetStretch()
-                mView.triggerLongSwipe = false
             }
             MotionEvent.ACTION_MOVE -> {
                 if (dragSlopExceeded(event.x, startX)) {
-                    mView.triggerLongSwipe = triggerLongSwipe
                     handleMoveEvent(event)
                 }
             }
             MotionEvent.ACTION_UP -> {
-                mView.triggerLongSwipe = triggerLongSwipe
                 when (currentState) {
                     GestureState.ENTRY -> {
                         if (
@@ -379,7 +372,6 @@ internal constructor(
                 velocityTracker = null
             }
             MotionEvent.ACTION_CANCEL -> {
-                mView.triggerLongSwipe = triggerLongSwipe
                 // Receiving a CANCEL implies that something else intercepted
                 // the gesture, i.e., the user did not cancel their gesture.
                 // Therefore, disappear immediately, with minimum fanfare.
@@ -520,10 +512,6 @@ internal constructor(
                 GestureState.INACTIVE -> stretchInactiveBackIndicator(gestureProgress)
                 else -> {}
             }
-        }
-
-        if (isLongSwipeEnabled) {
-            setTriggerLongSwipe(abs(xTranslation) > longSwipeThreshold)
         }
 
         setArrowStrokeAlpha(gestureProgress)
@@ -682,37 +670,12 @@ internal constructor(
         windowManager.addView(mView, layoutParams)
     }
 
-    override fun setLongSwipeEnabled(enabled: Boolean) {
-        longSwipeThreshold = if (enabled) MathUtils.min(
-            displaySize.x * 0.5f, layoutParams.width * 2.5f) else 0.0f
-        isLongSwipeEnabled = longSwipeThreshold > 0
-        setTriggerLongSwipe(isLongSwipeEnabled && triggerLongSwipe)
-    }
-
     override fun setBackArrowVisibility(enabled: Boolean) {
         backArrowVisibility = enabled
     }
 
     override fun setEdgeHapticEnabled(enabled: Boolean) {
         edgeHapticEnabled = enabled
-    }
-
-    private fun setTriggerLongSwipe(enabled: Boolean) {
-        if (triggerLongSwipe != enabled) {
-            triggerLongSwipe = enabled
-            if (edgeHapticEnabled) vibratorHelper.performHapticFeedback(
-                    mView,
-                    HapticFeedbackConstants.GESTURE_THRESHOLD_ACTIVATE
-            )
-            updateRestingArrowDimens()
-            // Whenever the trigger back state changes
-            // the existing translation animation should be cancelled
-            cancelFailsafe()
-            mView.cancelAnimations()
-            mView.triggerLongSwipe = triggerLongSwipe
-            updateConfiguration()
-            backCallback.setTriggerLongSwipe(triggerLongSwipe)
-        }
     }
 
     private fun isFlungAwayFromEdge(endX: Float, startX: Float = touchDeltaStartX): Boolean {
@@ -939,17 +902,13 @@ internal constructor(
             GestureState.COMMITTED -> {
                 // When flung, trigger back immediately but don't fire again
                 // once state resolves to committed.
-                if (previousState != GestureState.FLUNG) backCallback.triggerBack(false)
+                if (previousState != GestureState.FLUNG) backCallback.triggerBack()
             }
             GestureState.ENTRY,
             GestureState.INACTIVE -> {
-                setTriggerLongSwipe(false)
                 backCallback.setTriggerBack(false)
             }
             GestureState.ACTIVE -> {
-                if (triggerLongSwipe) {
-                    backCallback.triggerBack(false)
-                }
                 backCallback.setTriggerBack(true)
             }
             GestureState.GONE -> {}
