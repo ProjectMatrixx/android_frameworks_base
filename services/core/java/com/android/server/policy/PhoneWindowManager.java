a1c1a80bc19da8ccd8658d9319cc655659c23cf4
SuperDroidBond <superdroidbond@yahoo.com>
2024-02-01 04:07:18 +0900

    [SQUASH] Revert long swipe gesture implementation [1/2]
    Revert "Extends "Use double click effect for long swipe""
    
    This reverts commit 691f8bf7d8d7e8f8c798a871aa23a2285b5d3ad4.
    
    Revert "Use double click effect for long swipe"
    
    This reverts commit e3fd362d1a977a1891ee15270ae7efcca54e844c.
    
    Revert "SystemUI: EdgeBackGestureHandler: Cancel long swipe when cancelBack called"
    
    This reverts commit eeb44c1aa11b39572de0f07789ed4d8b14bdd5b7.
    
    Revert "SystemUI: BackPanel: Always add second arrow in front on long swipe"
    
    This reverts commit 8ce871e8f03c5f1fd8b4f00a61cd455748d77f6e.
    
    Revert "Implement edge long swipe gesture to new back gesture affordance"
    
    This reverts commit 7d1bc0bcf5efe30178b3a51d7531cb6bb50c2ae1.
    
    Revert "Implement edge long swipe gesture to new back gesture affordance"
    
    This reverts commit 3203c38a38ba6a8f5dd75fa716eda5c4566c757d.
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -660,7 +660,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private Action mAssistLongPressAction;
     private Action mAppSwitchPressAction;
     private Action mAppSwitchLongPressAction;
-    private Action mEdgeLongSwipeAction;
 
     // support for activating the lock screen while the screen is on
     private HashSet<Integer> mAllowLockscreenWhenOnDisplays = new HashSet<>();
@@ -821,8 +820,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private PendingIntent mTorchOffPendingIntent;
 
     private LineageHardwareManager mLineageHardware;
-
-    private boolean mLongSwipeDown;
 
     private class PolicyHandler extends Handler {
         @Override
@@ -1017,9 +1014,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
                     LineageSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(LineageSettings.System.getUriFor(
-                    LineageSettings.System.KEY_EDGE_LONG_SWIPE_ACTION), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
                     LineageSettings.System.HOME_WAKE_SCREEN), false, this,
@@ -2996,8 +2990,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final Resources res = mContext.getResources();
 
         // Initialize all assignments to sane defaults.
-        mEdgeLongSwipeAction = Action.NOTHING;
-
         mMenuPressAction = Action.MENU;
 
         mMenuLongPressAction = Action.fromIntSafe(res.getInteger(
@@ -3066,10 +3058,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mAppSwitchLongPressAction = Action.fromSettings(resolver,
                 LineageSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
                 mAppSwitchLongPressAction);
-
-        mEdgeLongSwipeAction = Action.fromSettings(resolver,
-                LineageSettings.System.KEY_EDGE_LONG_SWIPE_ACTION,
-                mEdgeLongSwipeAction);
 
         mShortPressOnWindowBehavior = SHORT_PRESS_WINDOW_NOTHING;
         if (mPackageManager.hasSystemFeature(FEATURE_PICTURE_IN_PICTURE)) {
@@ -5023,23 +5011,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_BACK: {
                 boolean isLongSwipe = (event.getFlags() & KeyEvent.FLAG_LONG_SWIPE) != 0;
                 logKeyboardSystemsEventOnActionUp(event, KeyboardLogEvent.BACK);
-
-                if (mLongSwipeDown && isLongSwipe && !down) {
-                    // Trigger long swipe action
-                    performKeyAction(mEdgeLongSwipeAction, event);
-                    // Reset long swipe state
-                    mLongSwipeDown = false;
-                    // Don't pass back press to app
-                    result &= ~ACTION_PASS_TO_USER;
-                    break;
-                }
-                mLongSwipeDown = isLongSwipe && down;
-                if (mLongSwipeDown) {
-                    // Don't pass back press to app
-                    result &= ~ACTION_PASS_TO_USER;
-                    break;
-                }
-
                 if (down) {
                     mBackKeyHandled = false;
 
