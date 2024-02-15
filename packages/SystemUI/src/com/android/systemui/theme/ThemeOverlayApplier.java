b29d4948e6ab1d3efa37fa1dba706d959681be8b
Subratam6296 <padraramesh420@gmail.com>
2024-02-15 07:37:06 +0530

    SystemUI: Add Switch Styles from Iconify [1/3]
    Change-Id: I8bf6fda7a5352cc49a791754c8cfbf95ec8b2d2d

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -129,6 +129,9 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_UI_STYLE_SYSUI =
             "android.theme.customization.style.systemui";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_SWITCH_STYLE =
+            "android.theme.customization.style.switch";
 
     /*
      * All theme customization categories used by the system, in order that they should be applied,
@@ -151,7 +154,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
             OVERLAY_CATEGORY_UI_STYLE_ANDROID,
             OVERLAY_CATEGORY_UI_STYLE_SETTINGS,
-            OVERLAY_CATEGORY_UI_STYLE_SYSUI);
+            OVERLAY_CATEGORY_UI_STYLE_SYSUI,
+            OVERLAY_CATEGORY_SWITCH_STYLE);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -166,7 +170,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_NAVBAR,
-            OVERLAY_CATEGORY_LOCK_CLOCK_FONT);
+            OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
+            OVERLAY_CATEGORY_SWITCH_STYLE);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -216,6 +221,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_WIFI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NAVBAR, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_LOCK_CLOCK_FONT, ANDROID_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_SWITCH_STYLE, SETTINGS_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
