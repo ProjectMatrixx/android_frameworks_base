57dd20ddbc8bf6c821b3d87a58782ec5aef97597
mukesh22584 <mks22584@gmail.com>
2024-03-20 14:37:09 +0530

    Revert "Allow adding space below IME on disabling navigation hint [1/3]"
    This reverts commit 9ae2491c648ed6ea9a7bb275b790752b897bcecb.
    
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -102,16 +102,12 @@ public class NavigationBarInflaterView extends FrameLayout implements TunerServi
             "lineagesystem:" + LineageSettings.System.NAVIGATION_BAR_HINT;
     private static final String OVERLAY_NAVIGATION_HIDE_HINT =
             "com.custom.overlay.systemui.gestural.hidden";
-    private static final String OVERLAY_NAVIGATION_HIDE_HINT_IME =
-            "com.custom.overlay.systemui.gestural.hidden_narrow_back";
     private static final String GESTURE_NAVBAR_LENGTH_MODE =
             "system:" + Settings.System.GESTURE_NAVBAR_LENGTH_MODE;
     private static final String GESTURE_NAVBAR_RADIUS =
             "system:" + Settings.System.GESTURE_NAVBAR_RADIUS;
     private static final String ENABLE_TASKBAR =
             "lineagesystem:" + LineageSettings.System.ENABLE_TASKBAR;
-    private static final String KEY_NAVIGATION_SPACE =
-            "system:" + Settings.System.NAVIGATION_BAR_IME_SPACE;
 
     private static class Listener implements NavigationModeController.ModeChangedListener {
         private final WeakReference<NavigationBarInflaterView> mSelf;
@@ -155,7 +151,6 @@ public class NavigationBarInflaterView extends FrameLayout implements TunerServi
     private boolean mIsHintEnabled;
     private int mHomeHandleWidthMode = 0;
     private boolean mIsTaskbarEnabled;
-    private boolean mShowImeSpace;
 
     public NavigationBarInflaterView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -222,7 +217,6 @@ public class NavigationBarInflaterView extends FrameLayout implements TunerServi
         Dependency.get(TunerService.class).addTunable(this, GESTURE_NAVBAR_LENGTH_MODE);
         Dependency.get(TunerService.class).addTunable(this, GESTURE_NAVBAR_RADIUS);
         Dependency.get(TunerService.class).addTunable(this, ENABLE_TASKBAR);
-        Dependency.get(TunerService.class).addTunable(this, KEY_NAVIGATION_SPACE);
     }
 
     @Override
@@ -252,10 +246,6 @@ public class NavigationBarInflaterView extends FrameLayout implements TunerServi
         } else if (ENABLE_TASKBAR.equals(key)) {
             mIsTaskbarEnabled =
                 TunerService.parseIntegerSwitch(newValue, isLargeScreen(mContext));
-            updateHint();
-        } else if (KEY_NAVIGATION_SPACE.equals(key)) {
-            mShowImeSpace =
-                TunerService.parseIntegerSwitch(newValue, false);
             updateHint();
         }
     }
@@ -330,23 +320,16 @@ public class NavigationBarInflaterView extends FrameLayout implements TunerServi
         final boolean state = mNavBarMode == NAV_BAR_MODE_GESTURAL && (!mIsHintEnabled ||
             mIsTaskbarEnabled);
         final int userId = ActivityManager.getCurrentUser();
-        String overlay = mShowImeSpace ? OVERLAY_NAVIGATION_HIDE_HINT_IME :
-            OVERLAY_NAVIGATION_HIDE_HINT;
         try {
-            if (mShowImeSpace) {
-                iom.setEnabled(OVERLAY_NAVIGATION_HIDE_HINT, false, userId);
-            } else {
-                iom.setEnabled(OVERLAY_NAVIGATION_HIDE_HINT_IME, false, userId);
-            }
-            iom.setEnabled(overlay, state, userId);
+            iom.setEnabled(OVERLAY_NAVIGATION_HIDE_HINT, state, userId);
             if (state) {
                 // As overlays are also used to apply navigation mode, it is needed to set
                 // our customization overlay to highest priority to ensure it is applied.
-                iom.setHighestPriority(overlay, userId);
+                iom.setHighestPriority(OVERLAY_NAVIGATION_HIDE_HINT, userId);
             }
         } catch (IllegalArgumentException | RemoteException e) {
             Log.e(TAG, "Failed to " + (state ? "enable" : "disable")
-                    + " overlay " + overlay + " for user " + userId);
+                    + " overlay " + OVERLAY_NAVIGATION_HIDE_HINT + " for user " + userId);
         }
     }
 
