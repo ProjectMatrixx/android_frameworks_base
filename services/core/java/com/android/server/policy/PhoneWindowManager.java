73dc1b118724bee2fa86d678f2d7be085beeabeb
Pranav Vashi <neobuddy89@gmail.com>
2024-08-20 22:49:59 +0530

    Revert "SwipeToScreenshot: Import MIUI implementation"
    This reverts commit fbd6268e36d5e087a63a3e040098a0ae4d706e56.
    
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -683,7 +683,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     ANBIHandler mANBIHandler;
     private boolean mANBIEnabled;
 
-    private SwipeToScreenshotListener mSwipeToScreenshot;
+    private OPGesturesListener mOPGestures;
     private boolean haveEnableGesture = false;
 
     // Tracks user-customisable behavior for certain key events
@@ -3469,13 +3469,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             boolean threeFingerGesture = Settings.System.getIntForUser(resolver,
                     Settings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT) == 1;
-            if (mSwipeToScreenshot != null) {
+            if (mOPGestures != null) {
                 if (haveEnableGesture != threeFingerGesture) {
                     haveEnableGesture = threeFingerGesture;
                     if (haveEnableGesture) {
-                        mWindowManagerFuncs.registerPointerEventListener(mSwipeToScreenshot, DEFAULT_DISPLAY);
+                        mWindowManagerFuncs.registerPointerEventListener(mOPGestures, DEFAULT_DISPLAY);
                     } else {
-                        mWindowManagerFuncs.unregisterPointerEventListener(mSwipeToScreenshot, DEFAULT_DISPLAY);
+                        mWindowManagerFuncs.unregisterPointerEventListener(mOPGestures, DEFAULT_DISPLAY);
                     }
                 }
             }
@@ -6954,7 +6954,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mVrManagerInternal.addPersistentVrModeStateListener(mPersistentVrModeListener);
         }
 
-        mSwipeToScreenshot = new SwipeToScreenshotListener(mContext, new SwipeToScreenshotListener.Callbacks() {
+        mOPGestures = new OPGesturesListener(mContext, new OPGesturesListener.Callbacks() {
             @Override
             public void onSwipeThreeFinger() {
                 interceptScreenshotChord(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER, 0 /*pressDelay*/);
