972b95fe7bfc3f2b27354ea317bd81089b46393b
mukesh22584 <mks22584@gmail.com>
2024-08-23 11:47:07 +0530

    Revert "base: Add haptic feedback to volume slider [1/2]"
    This reverts commit 14f26788a00d0750ae3544d088d48f259b54fedc.
    
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -115,8 +115,7 @@ public class SystemSettings {
                 Settings.System.NAVIGATION_BAR_IME_SPACE,
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
-                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
-                Settings.System.HAPTIC_ON_SLIDER
+                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
