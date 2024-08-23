PixysOS/frameworks_base

base: Extend showing Ambient instead of fully waking to single tap gesture [1/2]
Change-Id: I5e5bf835a1e8ecec59dbc1ee0563a6adf30ad6e9
Signed-off-by: aswin7469 <aswinas@pixysos.com>

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -126,6 +126,12 @@ public class AmbientDisplayConfiguration {
     }
 
     /** @hide */
+    public boolean tapGestureAmbient(int user) {
+        return boolSettingDefaultOff(Settings.Secure.DOZE_SINGLE_TAP_GESTURE_AMBIENT, user)
+                && tapGestureEnabled(user) && pulseOnNotificationEnabled(user);
+    }
+
+    /** @hide */
     public boolean tapSensorAvailable() {
         for (String tapType : tapSensorTypeMapping()) {
             if (!TextUtils.isEmpty(tapType)) {

packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -370,7 +370,9 @@ public class DozeTriggers implements DozeMachine.Part {
         if (reason == DozeLog.REASON_SENSOR_PICKUP &&
             mConfig.pickupGestureAmbient(UserHandle.USER_CURRENT) ||
             reason == DozeLog.REASON_SENSOR_DOUBLE_TAP
-            && mConfig.doubleTapGestureAmbient(UserHandle.USER_CURRENT)) {
+            && mConfig.doubleTapGestureAmbient(UserHandle.USER_CURRENT) ||
+            reason == DozeLog.REASON_SENSOR_SINGLE_TAP
+            && mConfig.singleTapGestureAmbient(UserHandle.USER_CURRENT)) {
             requestPulse(reason, true, null);
             return;
         }

ProjectMatrixx/android_frameworks_base

Signed-off-by: mukesh22584 <mks22584@gmail.com>

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -166,6 +166,12 @@ public class AmbientDisplayConfiguration {
     }
 
     /** @hide */
+    public boolean singleTapGestureAmbient(int user) {
+        return boolSettingDefaultOff(Settings.Secure.DOZE_SINGLE_TAP_GESTURE_AMBIENT, user)
+                && tapGestureEnabled(user) && pulseOnNotificationEnabled(user);
+    }
+
+    /** @hide */
     public boolean tapSensorAvailable() {
         for (String tapType : tapSensorTypeMapping()) {
             if (!TextUtils.isEmpty(tapType)) {

packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -378,7 +378,9 @@ public class DozeTriggers implements DozeMachine.Part {
         if (reason == DozeLog.REASON_SENSOR_PICKUP &&
             mConfig.pickupGestureAmbient(UserHandle.USER_CURRENT) ||
             reason == DozeLog.REASON_SENSOR_DOUBLE_TAP
-            && mConfig.doubleTapGestureAmbient(UserHandle.USER_CURRENT)) {
+            && mConfig.doubleTapGestureAmbient(UserHandle.USER_CURRENT) ||
+            reason == DozeLog.REASON_SENSOR_TAP
+            && mConfig.singleTapGestureAmbient(UserHandle.USER_CURRENT)) {
             requestPulse(reason, true, null);
             return;
         }