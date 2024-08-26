b3e604468a49d631ac3807b2f50f30cc6ab227d9
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-26 09:59:48 +0800

    PixelPropsUtils: Spoof Pixel XL to Snapchat
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -147,6 +147,10 @@ public final class PixelPropsUtils {
                 }
             }
         }
+        
+        if (packageName.equals("com.snapchat.android")) {
+            propsToChange.putAll(propsToChangePixelXL);
+        }
 
         if (packageName.equals("com.google.android.gms")) {
             if (SystemProperties.getBoolean(SPOOF_PIXEL_GMS, true)) {
