Evolution-X-AOSP/frameworks_base

Gestures nav: Add Partial Screenshot action
Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: Ie8045e35ecb19e4c07113865c6a092859aab9746

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1388,6 +1388,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 14: // Powermenu
                 EvolutionUtils.showPowerMenu();
                 break;
+            case 15: // Partial Screenshot
+                EvolutionUtils.takeScreenshot(false);
+                break;
         }
     }
 

ProjectMatrixx/android_frameworks_base

Signed-off-by: mukesh22584 <mks22584@gmail.com>
Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1395,6 +1395,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 14: // Powermenu
                 systemUtils.showPowerMenu();
                 break;
+            case 15: // Partial Screenshot
+                systemUtils.takeScreenshot(false);
+                break;
         }
     }
 