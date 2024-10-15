4e7689edb8d179b81ffe078ffde345bbaa4825de
ReStranger <strengerplayr@gmail.com>
2024-10-15 15:38:40 +0300

    PixelPropsUtils: Update fingerprint

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -58,11 +58,11 @@ public class PixelPropsUtils {
     private static final Map<String, String> DEFAULT_VALUES = Map.of(
         "BRAND", "google",
         "MANUFACTURER", "Google",
-        "DEVICE", "oriole",
-        "FINGERPRINT", "google/oriole_beta/oriole:15/AP41.240823.009/12329489:user/release-keys",
-        "MODEL", "Pixel 6",
-        "PRODUCT", "oriole_beta",
-        "DEVICE_INITIAL_SDK_INT", "31",
+        "DEVICE", "tokay",
+        "FINGERPRINT", "google/tokay_beta/tokay:15/AP41.240823.009/12329489:user/release-keys",
+        "MODEL", "Pixel 9",
+        "PRODUCT", "tokay_beta",
+        "DEVICE_INITIAL_SDK_INT", "25",
         "SECURITY_PATCH", "2024-09-05",
         "ID", "AP41.240823.009"
     );
